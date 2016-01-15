144755b05c2c453296f0822470e5b0bf0b0d5a67
wyicwx <wyicwx@gmail.com>
2016-01-15 18:52:45 +0800

    add unit test case

---
+++ test/bone/bonefile.js
@@ -0,0 +1,5 @@
+var bone = require('bone');
+var less = bone.require('../../');
+
+var dist = bone.dest('dist').cwd('~/');
+
---
+++ test/index.js
@@ -0,0 +1,26 @@
+'use strict';
+var assert = require('assert');
+var path = require('path');
+var bone = require('bone');
+
+bone.setup(path.join(__dirname, './raw'));
+require('./bone/bonefile.js');
+bone.run();
+
+var FileSystem = require('bone/lib/fs.js');
+var bonefs = FileSystem.fs;
+
+bone.status.test = true;
+bone.log.log('test start.');
+
+describe('less', function() {
+    it('correct', function(done) {
+        bonefs.readFile('~/dist/style.css', function(error, buffer) {
+            if(error) {
+                return done(false);
+            }
+
+
+        });
+    });
+});
